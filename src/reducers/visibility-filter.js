9ab0d11c5f9d340dc4ca5296eb6f14eaa25fb18e
harvest <wottinger@trhc.com>
2019-11-25 16:08:04 -0500

    remove consoles

--- src/reducers/visibility-filter.js
+++ src/reducers/visibility-filter.js
@@ -1,12 +1,8 @@
 import { SET_VISIBILITY_FILTER, VisibilityFilters } from '../actions'
 
 function visibilityFilter(state = VisibilityFilters.SHOW_ALL, action) {
-  console.log({ action })
-
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
-      console.log('*** INSIDE case SET_VISIBILITY_FILTER ***')
-      console.log('this is returned as state', action.payload)
       return action.payload
     default:
       return state
